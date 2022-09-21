1342ac7c6da295c1ed556daf52162bc8c6ae4824
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2022-09-21 19:26:45 +0400

    changing the order of iterators for easier testing

--- iternal/game/pack.go
+++ iternal/game/pack.go
@@ -6,8 +6,8 @@ func createInitialPack() pack {
 	b := pack{}
 
 	for d := 0; d < DecksNumber; d++ {
-		for i := MinNumber; i <= MaxNumber; i++ {
-			for _, c := range colors {
+		for _, c := range colors {
+			for i := MinNumber; i <= MaxNumber; i++ {
 				p := createPiece(i, c, false)
 				b = append(b, p)
 			}
