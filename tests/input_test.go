09b9520c47695d208f2b263f88e4a58eaf7ef57a
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2022-09-22 03:31:33 +0400

    input tests implemented

--- tests/input_test.go
+++ tests/input_test.go
@@ -214,7 +214,7 @@ func TestAddPieceAction(t *testing.T) {
 	action = game.ActionRequest{
 		Player:           "p1",
 		Action:           game.AddPiece,
-		AddedPieces:      []int{0},
+		AddedPieces:      []int{1},
 		UsedCombinations: []int{2},
 		TimerExceeded:    false,
 	}
@@ -420,5 +420,476 @@ func TestRemovePieceAction(t *testing.T) {
 
 	if g.HandSize("p1") != game.HandSize-3 {
 		t.Fatal("invalid action 3 changed hand size")
+	}
+}
+
+func TestReplacePieceAction(t *testing.T) {
+	g, err := game.NewTestGame()
+	if err != nil {
+		t.Fatalf("test game creation Fatal: %v", err)
+	}
+
+	action := game.ActionRequest{
+		Player:        "p1",
+		Action:        game.InitialMeld,
+		AddedPieces:   []int{9, 10, 11},
+		TimerExceeded: false,
+	}
+
+	j, _ := json.Marshal(&action)
+	g.HandleAction(j)
+
+	// Valid action
+	action = game.ActionRequest{
+		Player:           "p1",
+		Action:           game.ReplacePiece,
+		AddedPieces:      []int{0},
+		RemovedPiece:     0,
+		UsedCombinations: []int{1},
+		TimerExceeded:    false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	response, err := g.HandleAction(j)
+	if err != nil {
+		t.Fatalf("can't handle action: %v", err)
+	}
+
+	var ar game.ActionResponse
+	err = json.Unmarshal(response, &ar)
+	if err != nil {
+		t.Fatalf("can't parse action response: %v", err)
+	}
+
+	if g.FieldSize() != 1 {
+		t.Fatal("removing piece changed field size")
+	}
+
+	if g.HandSize("p1") != game.HandSize-3 {
+		t.Fatal("wrong hand size after removing piece")
+	}
+
+	// Invalid action 1: trying to add piece before initial meld made
+	action = game.ActionRequest{
+		Player:           "p2",
+		Action:           game.ReplacePiece,
+		AddedPieces:      []int{0},
+		RemovedPiece:     0,
+		UsedCombinations: []int{1},
+		TimerExceeded:    false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	_, err = g.HandleAction(j)
+	if err == nil {
+		t.Fatalf("invalid action was handled: %v", err)
+	}
+
+	if g.FieldSize() != 1 {
+		t.Fatal("invalid action 1 changed field size")
+	}
+
+	if g.HandSize("p2") != game.HandSize {
+		t.Fatal("invalid action 1 changed hand size")
+	}
+
+	// Invalid action 2: wrong combination number
+	action = game.ActionRequest{
+		Player:           "p1",
+		Action:           game.ReplacePiece,
+		AddedPieces:      []int{0},
+		RemovedPiece:     0,
+		UsedCombinations: []int{10},
+		TimerExceeded:    false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	_, err = g.HandleAction(j)
+	if err == nil {
+		t.Fatalf("invalid action 2 was handled")
+	}
+
+	if g.FieldSize() != 1 {
+		t.Fatal("invalid action 2 changed field size")
+	}
+
+	if g.HandSize("p1") != game.HandSize-3 {
+		t.Fatal("invalid action 2 changed hand size")
+	}
+
+	// Invalid action 3: wrong resulting combination
+	action = game.ActionRequest{
+		Player:           "p1",
+		Action:           game.ReplacePiece,
+		AddedPieces:      []int{1},
+		RemovedPiece:     0,
+		UsedCombinations: []int{1},
+		TimerExceeded:    false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	_, err = g.HandleAction(j)
+	if err == nil {
+		t.Fatalf("invalid action 3 was handled")
+	}
+
+	if g.FieldSize() != 1 {
+		t.Fatal("invalid action 3 changed field size")
+	}
+
+	if g.HandSize("p1") != game.HandSize-3 {
+		t.Fatal("invalid action 3 changed hand size")
+	}
+}
+
+func TestAddCombinationAction(t *testing.T) {
+	g, err := game.NewTestGame()
+	if err != nil {
+		t.Fatalf("test game creation Fatal: %v", err)
+	}
+
+	action := game.ActionRequest{
+		Player:        "p1",
+		Action:        game.InitialMeld,
+		AddedPieces:   []int{10, 11, 12},
+		TimerExceeded: false,
+	}
+
+	j, _ := json.Marshal(&action)
+	g.HandleAction(j)
+
+	// Valid action
+	action = game.ActionRequest{
+		Player:        "p1",
+		Action:        game.AddCombination,
+		AddedPieces:   []int{0, 1, 2},
+		TimerExceeded: false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	response, err := g.HandleAction(j)
+	if err != nil {
+		t.Fatalf("can't handle action: %v", err)
+	}
+
+	var ar game.ActionResponse
+	err = json.Unmarshal(response, &ar)
+	if err != nil {
+		t.Fatalf("can't parse action response: %v", err)
+	}
+
+	if g.FieldSize() != 2 {
+		t.Fatal("removing piece changed field size")
+	}
+
+	if g.HandSize("p1") != game.HandSize-6 {
+		t.Fatal("wrong hand size after removing piece")
+	}
+
+	// Invalid action 1: trying to add piece before initial meld made
+	action = game.ActionRequest{
+		Player:        "p2",
+		Action:        game.AddCombination,
+		AddedPieces:   []int{0, 1, 2},
+		TimerExceeded: false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	_, err = g.HandleAction(j)
+	if err == nil {
+		t.Fatalf("invalid action was handled: %v", err)
+	}
+
+	if g.FieldSize() != 2 {
+		t.Fatal("invalid action 1 changed field size")
+	}
+
+	if g.HandSize("p2") != game.HandSize {
+		t.Fatal("invalid action 1 changed hand size")
+	}
+
+	// Invalid action 2: wrong combination
+	action = game.ActionRequest{
+		Player:        "p1",
+		Action:        game.AddCombination,
+		AddedPieces:   []int{0, 3, 5},
+		TimerExceeded: false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	_, err = g.HandleAction(j)
+	if err == nil {
+		t.Fatalf("invalid action 2 was handled")
+	}
+
+	if g.FieldSize() != 2 {
+		t.Fatal("invalid action 2 changed field size")
+	}
+
+	if g.HandSize("p1") != game.HandSize-6 {
+		t.Fatal("invalid action 2 changed hand size")
+	}
+}
+
+func TestConcatCombinationsAction(t *testing.T) {
+	g, err := game.NewTestGame()
+	if err != nil {
+		t.Fatalf("test game creation Fatal: %v", err)
+	}
+
+	action := game.ActionRequest{
+		Player:        "p1",
+		Action:        game.InitialMeld,
+		AddedPieces:   []int{10, 11, 12},
+		TimerExceeded: false,
+	}
+
+	j, _ := json.Marshal(&action)
+	g.HandleAction(j)
+
+	action = game.ActionRequest{
+		Player:        "p1",
+		Action:        game.AddCombination,
+		AddedPieces:   []int{7, 8, 9},
+		TimerExceeded: false,
+	}
+
+	j, _ = json.Marshal(&action)
+	g.HandleAction(j)
+
+	action = game.ActionRequest{
+		Player:        "p1",
+		Action:        game.AddCombination,
+		AddedPieces:   []int{4, 5, 6},
+		TimerExceeded: false,
+	}
+
+	j, _ = json.Marshal(&action)
+	g.HandleAction(j)
+
+	action = game.ActionRequest{
+		Player:        "p1",
+		Action:        game.AddCombination,
+		AddedPieces:   []int{1, 2, 3},
+		TimerExceeded: false,
+	}
+
+	j, _ = json.Marshal(&action)
+	g.HandleAction(j)
+
+	// Valid action
+	action = game.ActionRequest{
+		Player:           "p1",
+		Action:           game.ConcatCombinations,
+		UsedCombinations: []int{1, 2},
+		TimerExceeded:    false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	response, err := g.HandleAction(j)
+	if err != nil {
+		t.Fatalf("can't handle action: %v", err)
+	}
+
+	var ar game.ActionResponse
+	err = json.Unmarshal(response, &ar)
+	if err != nil {
+		t.Fatalf("can't parse action response: %v", err)
+	}
+
+	if g.FieldSize() != 3 {
+		t.Fatal("removing piece changed field size")
+	}
+
+	if g.HandSize("p1") != game.HandSize-12 {
+		t.Fatal("wrong hand size after removing piece")
+	}
+
+	// Invalid action 1: trying to concat combinations before initial meld made
+	action = game.ActionRequest{
+		Player:           "p2",
+		Action:           game.ConcatCombinations,
+		UsedCombinations: []int{3, 4},
+		TimerExceeded:    false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	_, err = g.HandleAction(j)
+	if err == nil {
+		t.Fatalf("invalid action was handled: %v", err)
+	}
+
+	if g.FieldSize() != 3 {
+		t.Fatal("invalid action 1 changed field size")
+	}
+
+	if g.HandSize("p2") != game.HandSize {
+		t.Fatal("invalid action 1 changed hand size")
+	}
+
+	// Invalid action 2: wrong combination
+	action = game.ActionRequest{
+		Player:           "p1",
+		Action:           game.ConcatCombinations,
+		UsedCombinations: []int{4, 5},
+		TimerExceeded:    false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	_, err = g.HandleAction(j)
+	if err == nil {
+		t.Fatalf("invalid action 2 was handled")
+	}
+
+	if g.FieldSize() != 3 {
+		t.Fatal("invalid action 2 changed field size")
+	}
+
+	if g.HandSize("p1") != game.HandSize-12 {
+		t.Fatal("invalid action 2 changed hand size")
+	}
+}
+
+func TestSplitCombinationsAction(t *testing.T) {
+	g, err := game.NewTestGame()
+	if err != nil {
+		t.Fatalf("test game creation Fatal: %v", err)
+	}
+
+	action := game.ActionRequest{
+		Player:        "p1",
+		Action:        game.InitialMeld,
+		AddedPieces:   []int{7, 8, 9, 10, 11, 12},
+		TimerExceeded: false,
+	}
+
+	j, _ := json.Marshal(&action)
+	g.HandleAction(j)
+
+	// Valid action
+	action = game.ActionRequest{
+		Player:           "p1",
+		Action:           game.SplitCombination,
+		SplitBeforeIndex: 3,
+		UsedCombinations: []int{1},
+		TimerExceeded:    false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	response, err := g.HandleAction(j)
+	if err != nil {
+		t.Fatalf("can't handle action: %v", err)
+	}
+
+	var ar game.ActionResponse
+	err = json.Unmarshal(response, &ar)
+	if err != nil {
+		t.Fatalf("can't parse action response: %v", err)
+	}
+
+	if g.FieldSize() != 2 {
+		t.Fatal("removing piece changed field size")
+	}
+
+	if g.HandSize("p1") != game.HandSize-6 {
+		t.Fatal("wrong hand size after removing piece")
+	}
+
+	// Invalid action 1: trying to split combinations before initial meld made
+	action = game.ActionRequest{
+		Player:           "p2",
+		Action:           game.SplitCombination,
+		SplitBeforeIndex: 2,
+		UsedCombinations: []int{2},
+		TimerExceeded:    false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	_, err = g.HandleAction(j)
+	if err == nil {
+		t.Fatalf("invalid action was handled: %v", err)
+	}
+
+	if g.FieldSize() != 2 {
+		t.Fatal("invalid action 1 changed field size")
+	}
+
+	if g.HandSize("p2") != game.HandSize {
+		t.Fatal("invalid action 1 changed hand size")
+	}
+
+	// Invalid action 2: wrong combination
+	action = game.ActionRequest{
+		Player:           "p1",
+		Action:           game.SplitCombination,
+		SplitBeforeIndex: 1,
+		UsedCombinations: []int{2},
+		TimerExceeded:    false,
+	}
+
+	j, err = json.Marshal(&action)
+	if err != nil {
+		t.Fatalf("can't convert action to json: %v", err)
+	}
+
+	_, err = g.HandleAction(j)
+	if err == nil {
+		t.Fatalf("invalid action 2 was handled")
+	}
+
+	if g.FieldSize() != 2 {
+		t.Fatal("invalid action 2 changed field size")
+	}
+
+	if g.HandSize("p1") != game.HandSize-6 {
+		t.Fatal("invalid action 2 changed hand size")
 	}
 }
